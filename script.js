9beceb06a09492ce554270120d8cd8fa34f75e6d
Alyssa <ajmeller@github.com>
2020-12-09 21:58:37 -0500

    UI and started logic

---
+++ script.js
@@ -0,0 +1,29 @@
+var startingBudget = prompt("Please enter your starting budget.", "0.00");
+
+document.getElementById("startingBudget").innerHTML = startingBudget;
+document.getElementById("remainingBudget").innerHTML = startingBudget;
+
+let remainingBudget = document.getElementById("remainingBudget");
+
+const expenseForm = document.getElementById("expenseForm");
+
+function budgetSubtract(remainingBudget, price) {
+  let updatedTotal = remainingBudget - price;
+  document.getElementById("remainingBudget").innerHTML = updatedTotal;
+}
+
+expenseForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const expenseData = new FormData(e.target);
+  const selectType = document.getElementById("expenseType");
+  const amount = parseFloat(expenseData.get("expenseAmount"));
+
+  const expenseOrder = {
+    description: expenseData.get("expenseDescription"),
+    category: selectType.value,
+    amount: amount,
+  };
+
+  budgetSubtract(remainingBudget, expenseOrder.amount);
+});
