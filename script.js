704c3f7d8d87056be06740dee296dfee264ac396
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2020-12-10 21:06:12 -0500

    added remaining javascript functions

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
 var startingBudget = prompt("Please enter your starting budget.", "0.00");
 
+document.getElementById("remainingBudget").innerHTML = startingBudget
 document.getElementById("startingBudget").innerHTML = startingBudget;
-document.getElementById("remainingBudget").innerHTML = startingBudget;
 
-let remainingBudget = document.getElementById("remainingBudget");
+function budgetSubtract(expenseAmount) {
+  let remainingBudget = document.getElementById("remainingBudget");
+  let updatedTotal = parseFloat(remainingBudget.innerHTML) - expenseAmount;
+  remainingBudget.innerHTML = updatedTotal;
+}
+
+function totalExpenses(expenseAmount) {
+  let totalcash =  document.getElementById("totalcash");
+  let totalAmount = parseFloat(totalcash.innerHTML) + expenseAmount;
+  totalcash.innerHTML = totalAmount;
+}
+
+function addToCategory(category, amount) {
+  let entcash = document.getElementById("entcash").innerHTML;
+  let foodcash = document.getElementById("foodcash").innerHTML;
+  let clothcash = document.getElementById("clothcash").innerHTML;
+  let billcash = document.getElementById("billcash").innerHTML;
+
+  entcash = parseFloat(entcash);
+  foodcash = parseFloat(foodcash);
+  clothcash = parseFloat(clothcash);
+  billcash = parseFloat(billcash);
+
+  if(category === 'entertainment') {
+    let newent = entcash + amount;
+    document.getElementById("entcash").innerHTML = newent;
+  } else if (category === 'food') {
+    let newfood = foodcash + amount;
+    document.getElementById("foodcash").innerHTML = newfood;
+  } else if (category === 'clothing') {
+    let newcloth = clothcash + amount;
+    document.getElementById("clothcash").innerHTML = newcloth;
+  } else if (category === 'bills') {
+    let newbill = billcash + amount;
+    document.getElementById("billcash").innerHTML = newbill;
+  }
+}
+
+function overdraft() {
+  let remainingBudget = document.getElementById("remainingBudget");
+  let remainingBudgetAmount = parseInt(remainingBudget.innerHTML);
+  if(remainingBudgetAmount <= 0) {
+    alert("Cannot purchase additional items.");
+    document.getElementById('expense-submit').setAttribute("disabled", true);
+  }
+}
+
+function logExpense(expenseObj){
+  const newDataRow = document.createElement("tr");
+  const descriptionCell = document.createElement("td");
+  descriptionCell.textContent = expenseObj.description;
+  newDataRow.appendChild(descriptionCell);
+  const categoryCell = document.createElement("td");
+  categoryCell.textContent = expenseObj.category;
+  newDataRow.appendChild(categoryCell);
+  const amountCell = document.createElement('td');
+  amountCell.textContent = "$" + expenseObj.amount;
+  newDataRow.appendChild(amountCell);
+  document.getElementById("expenseTable").appendChild(newDataRow);
+}
 
 const expenseForm = document.getElementById("expenseForm");
-
-function budgetSubtract(remainingBudget, expenseAmount) {
-  let updatedTotal = remainingBudget - expenseAmount;
-  document.getElementById("remainingBudget").innerHTML = updatedTotal;
-}
 
 expenseForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -19,11 +73,19 @@ expenseForm.addEventListener("submit", (e) => {
   const selectType = document.getElementById("expenseType");
   const amount = parseFloat(expenseData.get("expenseAmount"));
 
-  const expenseOrder = {
+  const expenseObj = {
     description: expenseData.get("expenseDescription"),
     category: selectType.value,
     amount: amount,
   };
 
-  budgetSubtract(remainingBudget, expenseOrder.amount);
-});
+  budgetSubtract(expenseObj.amount);
+
+  addToCategory(expenseObj.category, expenseObj.amount);
+
+  totalExpenses(expenseObj.amount);
+
+  logExpense(expenseObj);
+
+  overdraft();
+})
